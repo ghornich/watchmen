a3d9234f1a3d6cbaf75af18b41e2778b77945de2
Gergely Hornich <hornich.geri@gmail.com>
2017-07-08 17:25:14 +0200

    First commit

---
+++ cli.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+var watchmen=require('./watchmen')
+var argv=require('minimist')(process.argv.slice(2))
+
+// TODO help, -h --help /h /help ? /?
+
+var paths,cmd
+
+cmd=argv._[0]
+paths=argv._.slice(1)
+
+if (paths.length===0){paths=['./']}
+
+if (!cmd){console.log('Error: cmd is missing');return}
+
+watchmen(paths, cmd)
---
+++ watchmen.js
@@ -0,0 +1,81 @@
+var fs=require('fs')
+var exec=require('child_process').exec
+var pathlib=require('path')
+
+// TODO
+// add ignore regex capability
+// multiple paths (glob?)
+// convert to class
+
+// TODO ignore same consecutive changes for file, with customizable throttle time
+
+var prevChangePath=null
+var prevChangeTime=0
+var CHANGE_THROTTLE_MS=500
+
+module.exports=function watchmen(paths, cmd){
+
+    var running = false
+
+    paths=paths||['./']
+
+    console.log('-- Watchmen --')
+    console.log('paths: called for path(s) "' + paths.join(', ') + '", command: "'+cmd+'"')
+
+    var onChange = function(path, eventType, filename){
+        console.log('onChange called: ', path, eventType, filename)
+
+        var prevent=false
+
+        if (prevChangePath === path && Date.now() - prevChangeTime < CHANGE_THROTTLE_MS){
+            prevent=true
+        }
+
+        prevChangePath=path
+        prevChangeTime=Date.now()
+        if(prevent){
+            console.log('same event detected for path "'+path+'" within timeout, returning')
+            return
+        }
+
+        if (running) {
+            console.log('command is running, returning')
+            return
+        }
+
+        running = true
+
+        console.log('not running: executing command')
+
+        exec(cmd, function(err,sout,serr){
+            if (err||sout||serr) console.log(err||'',sout||'',serr||'')
+
+            console.log('command finished')
+
+            running = false
+        })
+    }
+
+    paths.forEach(p=>{
+        var absPath = pathlib.resolve(p)
+
+        fs.watch(absPath, { recursive: true }, onChange.bind(null, p))
+    })
+
+}
+
+
+// function debounce(func, wait, immediate) {
+//     var timeout;
+//     return function() {
+//         var context = this, args = arguments;
+//         var later = function() {
+//             timeout = null;
+//             if (!immediate) func.apply(context, args);
+//         };
+//         var callNow = immediate && !timeout;
+//         clearTimeout(timeout);
+//         timeout = setTimeout(later, wait);
+//         if (callNow) func.apply(context, args);
+//     };
+// }
